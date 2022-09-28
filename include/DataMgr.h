1747fcf5c9b51727ef9e6a5730abc8e74f695d56
Eideann <aingeru.scotland@gmail.com>
2022-09-28 19:58:01 +0200

    DataMgr added and few other corrections.

---
+++ include/DataMgr.h
@@ -0,0 +1,14 @@
+//
+// Created by Eideann on 28/9/22.
+//
+
+#ifndef CACHESIM_DATAMGR_H
+#define CACHESIM_DATAMGR_H
+
+
+class DataMgr {
+
+};
+
+
+#endif //CACHESIM_DATAMGR_H
---
+++ src/DataMgr.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Eideann on 28/9/22.
+//
+
+#include "DataMgr.h"
